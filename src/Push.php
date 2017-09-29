cc7a91ce7a7536ade363f4762e37c0ba0239a4c5
Jan Östlund <janne@eastwest.se>
2017-09-29 16:30:33 +0200

    Add comments

--- src/Push.php
+++ src/Push.php
@@ -7,8 +7,16 @@ use GuzzleHttp\Client;
 
 class Push
 {
-    const BASE_URL = 'http://pusher.dev';
+    /**
+     * URL to F-Bar Push server
+     */
+    const BASE_URL = 'https://pusher.eastwest.se';
     
+    /**
+     * Construct Push Notification
+     * @param string $message  Message sent to client
+     * @param string $deviceId Unique device ID for receiver
+     */
     public function __construct($message, $deviceId = null) 
     {
         $this->message = $message;
@@ -19,11 +27,19 @@ class Push
         return $this;
     }
 
+    /**
+     * Construct URL to F-Bar Push server
+     * @return String
+     */
     public function url()
     {
         return sprintf('%s/api/v1/device/%s/push', ltrim(self::BASE_URL, '/'), $this->deviceId);
     }
 
+    /**
+     * Construct Guzzle parameters
+     * @return array
+     */
     public function parameters() 
     {
         return [
@@ -33,6 +49,10 @@ class Push
         ];
     }
 
+    /**
+     * Create and send request with Guzzle
+     * @return [type] [description]
+     */
     public function sendRequest() 
     { 
         $result = (new Client())
@@ -41,6 +61,10 @@ class Push
         return $result;
     }
 
+    /**
+     * Check that Device ID is not null, or throw exeption
+     * @return null
+     */
     protected function guardAgaistNullDeviceId() 
     {
         if($this->deviceId == null) {
